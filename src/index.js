aa1a902ba3fcac580767f0336cff351633a48854
Luom Pham <luompham95@gmail.com>
2022-11-20 15:31:40 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 3000;
 
 
+// maybe write: const route = require('./routes/index.js');
+// or if it is an index.js file, it will be loaded automatically
+const route = require('./routes');
+
 // app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, 'public')));
 // http logger
@@ -19,23 +23,8 @@ app.set('view engine', '.hbs');
 
 app.set('views', './src/resources/views');
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-
-
-app.get('/news', (req, res) => {
-    console.log(req.query.q)
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    console.log(req.query.q)
-    res.render('search');
-});
-
-
+//Routes init
+route(app);
 
 
 app.listen(port, () => {
